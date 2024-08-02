d9531d6a204a397851f773689cb637092e3ba91f
Francisco Soto <panchon.ues@gmail.com>
2024-08-02 13:21:13 -0700

    animationsHook custom

--- src/presentation/screens/animations/Animation101Screen.tsx
+++ src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,47 +1,11 @@
 import { View, Text, StyleSheet, Pressable, Animated, Easing } from 'react-native';
 import { colors } from '../../../config/theme/theme';
-import { useRef } from 'react';
+import { useAnimation } from '../../hooks/useAnimation';
 
 
 export const Animation101Screen = () => {
 
-  const animatedOpacity = useRef(new Animated.Value(0)).current;
-  const animatedTop = useRef(new Animated.Value(0)).current;
-
-  const fadeIn = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(()=>console.log('fadeIn ended'));
-  }
-  const fadeOut = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(()=>{
-      animatedTop.resetAnimation();
-      console.log('fadeOut ended');
-    });
-  }
-
-  const bounceIn = () => {
-    Animated.timing(animatedTop, {
-      toValue: 100,
-      duration: 300,
-      useNativeDriver: true,
-      easing: Easing.elastic(1),
-    }).start(()=>console.log('bounce'));
-  }
-
-  const bounceOut = () => {
-    Animated.timing(animatedTop, {
-      toValue: -100,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(()=>console.log('bounce'));
-  }
+  const {fadeIn, fadeOut, animatedOpacity, animatedTop, startMovingTop } = useAnimation();
 
   return (
     <View style={styles.container}>
@@ -57,19 +21,26 @@ export const Animation101Screen = () => {
         ]}
       />
 
-      <Pressable onPress={fadeIn} style={{marginTop:10}}>
+      <Pressable onPress={()=>{
+        fadeIn({});
+        startMovingTop({
+          initialPosition:-100,
+          easing: Easing.elastic(1),
+          duration: 750
+        })
+       }} style={{marginTop:10}}>
         <Text>FadeIn</Text>
       </Pressable>
-      <Pressable onPress={fadeOut} style={{marginTop:10}}>
+      <Pressable onPress={()=>fadeOut({})} style={{marginTop:10}}>
         <Text>FadeOut</Text>
       </Pressable>
 
-      <Pressable onPress={bounceIn} style={{marginTop:10}}>
+      {/* <Pressable onPress={bounceIn} style={{marginTop:10}}>
         <Text>BounceIn</Text>
       </Pressable>
       <Pressable onPress={bounceOut} style={{marginTop:10}}>
         <Text>BounceOut</Text>
-      </Pressable>
+      </Pressable> */}
     </View>
   );
 }
