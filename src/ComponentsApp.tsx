9d5ed51384f8cc1932e1461f393c57d98dd18145
Francisco Soto <panchon.ues@gmail.com>
2024-08-06 13:32:20 -0700

    reactNavigation customTheme

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,26 +1,41 @@
 import 'react-native-gesture-handler';
 
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { Navigator } from './presentation/navigator/Navigator';
-import { ThemeProvider } from './presentation/context/ThemeContext';
+import { ThemeContext, ThemeProvider } from './presentation/context/ThemeContext';
 
-const AppState = ({children}:PropsWithChildren) => {
+import { CustomDarkTheme, CustomDefaultTheme } from './config/theme/NavigationTheme';
+
+
+
+
+const AppNavigation = ({children}:PropsWithChildren) => {
+
+  const { isDark } =useContext(ThemeContext);
+
   return (
-    <NavigationContainer>
-      <ThemeProvider>
-        { children }
-      </ThemeProvider>
+    <NavigationContainer theme={isDark ? CustomDarkTheme:CustomDefaultTheme}>
+      {children}
     </NavigationContainer>
+  );
+}
+
+
+const AppTheme = ({children}: PropsWithChildren) => {
+  return (
+    <ThemeProvider>
+      <AppNavigation>{children}</AppNavigation>
+    </ThemeProvider>
   );
 }
 
 
 export const ComponentsApp = () => {
   return (
-    <AppState>
+    <AppTheme>
       <Navigator />
-    </AppState>
+    </AppTheme>
   );
 }
